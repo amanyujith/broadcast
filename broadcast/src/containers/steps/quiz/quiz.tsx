ea81dc3d5eb934d46fc2e37fffd3c01da1e2216f
amanyujith <56426504+amanyujith@users.noreply.github.com>
2024-10-08 15:17:17 +0530

    highilghted previous answers

--- broadcast/src/containers/steps/quiz/quiz.tsx
+++ broadcast/src/containers/steps/quiz/quiz.tsx
@@ -3,13 +3,17 @@ import quizData from "../JSON/quizData";
 import Modal from "@/components/atom/modal/Modal";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button/button";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 const  Quiz =()=> {
     const navigate = useNavigate();
     const [score , setScore] = useState(0);
     const [currentQuestionIndex , setCurrentQuestionIndex] = useState(0);
     const [selectedAnswer , setSelectedAnswer] = useState("");
     const [modal , setModal] = useState(false);
-    const [correctAnswers , setCorrectAnswers] = useState<{ [key: number]: boolean }>({})
+    // const [correctAnswers , setCorrectAnswers] = useState<{ [key: number]: boolean }>({});
+    const [correctAnswers, setCorrectAnswers] = useState<{ [key: number]: string }>({});
+
+
     // const [isSaveDisabled , setIsSaveDisabled] = useState(false) ;
     const currentQuestion = quizData.questions[currentQuestionIndex];
     const questionNumbers = Array.from({ length: quizData.questions.length }, (_, i) => i + 1);
@@ -28,27 +32,27 @@ const  Quiz =()=> {
             //     setScore(score)
             // }
             const isCorrect = selectedAnswer === currentQuestion.answer;
-            const wasCorrectBefore = correctAnswers[currentQuestionIndex];
+            // const wasCorrectBefore = correctAnswers[currentQuestionIndex];
+            setCorrectAnswers({
+                ...correctAnswers,
+                [currentQuestionIndex]:selectedAnswer
+            });
 
             if(isCorrect){
-                if(!wasCorrectBefore){
+                if(!correctAnswers[currentQuestionIndex]){
                     setScore(score+1)
                 }
             }
             else{
-                if(wasCorrectBefore){
+                if(correctAnswers[currentQuestionIndex]){
                     setScore(score-1)
                 }
             }
-            setCorrectAnswers({
-                ...correctAnswers,[currentQuestionIndex]:isCorrect
-            });
+            
             console.log("score22222",score);
             
             if(currentQuestionIndex < quizData.questions.length-1){
                 setCurrentQuestionIndex(currentQuestionIndex+1)
-            }else{
-                setModal(true);
             }
             // setIsSaveDisabled(false);
             setSelectedAnswer("")
@@ -76,9 +80,13 @@ const  Quiz =()=> {
     <p><span>{currentQuestionIndex + 1}. </span>{currentQuestion.question}</p>
         {Object.entries(currentQuestion.options)?.map(([key, option]) => (
           <div key={key} className="option ">
-            <Button className={`optionButton my-2 whitespace-normal shadow-none w-full p-7 ${
-    selectedAnswer === key ? 'bg-blue-400 text-white hover:bg-blue-500' : 'bg-red-50 hover:bg-red-100'
-  } hover:border-black  `}
+            <Button  className={`optionButton my-2 whitespace-normal shadow-none w-full p-7 ${
+        selectedAnswer === key
+          ? 'bg-blue-400 text-white hover:bg-blue-500' 
+          : correctAnswers[currentQuestionIndex] === key
+          ? 'bg-green-400 text-white hover:bg-green-500' 
+          : 'bg-red-50 hover:bg-red-100' 
+      } hover:border-black`}
             onClick={()=>{setSelectedAnswer(key); }}
             >
                 {option}
@@ -93,16 +101,7 @@ const  Quiz =()=> {
             <label>{`${key}: ${option}`}</label> */}
           </div>
         ))}
-    {/* <button onClick={handleSave} className="bg-slate-300"  disabled={!selectedAnswer}>Save</button> */}
-    <Button onClick={()=>{handleSave() }}
-        variant="default"
-        size="default"
-        disabled={!selectedAnswer}
-        className="text-black font-bold  bg-white"
-        >
-        SAVE
-    </Button>
-    <Modal
+        <Modal
         isOpen={modal}
         onClose={closeModal}
         title="Quiz Completed"
@@ -113,8 +112,21 @@ const  Quiz =()=> {
       >
         <p className="text-center">Your total score is: {score}</p>
       </Modal>
+    {/* <button onClick={handleSave} className="bg-slate-300"  disabled={!selectedAnswer}>Save</button> */}
+    <div className="flex flex-col">
+        <div className=" flex justify-between items-center">
+    <Button onClick={()=>{handleSave() }}
+        variant="default"
+        size="default"
+        disabled={!selectedAnswer}
+        className="text-black font-bold  bg-white"
+        >
+        SAVE
+    </Button>
+    
       
       {/* <button onClick={()=>setCurrentQuestionIndex(currentQuestionIndex-1)} className="bg-slate-300">Prev</button> */}
+      <div className=" flex justify-center items-center">
       <Button 
                  onClick={()=>handlePrevClick()}
                 variant="secondary"
@@ -122,28 +134,46 @@ const  Quiz =()=> {
                 disabled={currentQuestionIndex === 0} 
                 className="m-2 bg-yellow-100"
             >
-                Prev
+                <ChevronLeft/>
+            
             </Button>
             <Button 
                 onClick={()=>handleNextClick()} 
                 variant="secondary"
-                size="default"
+                
                 disabled={currentQuestionIndex === quizData.questions.length-1} 
                 className="m-2 bg-yellow-100"
             >
-                Next
+               
+                <ChevronRight size={20}
+                />
             </Button>
+            </div>
+
+            <Button onClick={()=>setModal(true)} 
+            size="default"
+            >
+                Submit
+            </Button>
+            </div>
+            <div className="grid grid-cols-5">
       {questionNumbers.map((number)=>(
         <Button 
         key={number} 
-        onClick={() => setCurrentQuestionIndex(number - 1)} 
+        onClick={() => {setCurrentQuestionIndex(number - 1);setSelectedAnswer("")}} 
         variant="outline"
         size="sm"
-        className={`bg-slate-300 p-2 ${currentQuestionIndex === number - 1 ? "bg-blue-400" : ""} m-2`}
+        className={`p-2 m-2 ${
+            correctAnswers[number - 1] 
+              ? "bg-green-400 text-white"
+              : "bg-slate-300"
+            } ${currentQuestionIndex === number - 1 ? "bg-blue-400 text-white" : ""}`}
     >
         {number}
     </Button>
       ))}
+      </div>
+      </div>
   </div>
   </div>
   )
